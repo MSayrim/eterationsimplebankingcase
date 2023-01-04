476006f2cab58b5052c12097ae9e92df30626c60
muratsa <muratsayrim@hotmail.com.tr>
2023-01-04 03:51:49 +0300

    cleaned unused structure

--- src/main/java/com/example/eterationsimplebankingcase/dto/AccountRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.eterationsimplebankingcase.dto;
-
-import lombok.Data;
-
-@Data
-public class AccountRequestDTO {
-    private String owner;
-    private String accountNumber;
-}
